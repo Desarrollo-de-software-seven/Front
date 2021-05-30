42daf470845e45883c64fec798d30d32ba992f3a
tomvalenz <tpvalenzuela@uc.cl>
2021-05-30 19:26:30 -0400

    added button styles and fixed white bars in waiting view

--- src/components/assistant.js
+++ src/components/assistant.js
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Box from '@material-ui/core/Box';
+// import Box from '@material-ui/core/Box';
 
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-
-import Avatar from '@material-ui/core/Avatar';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import Paper from '@material-ui/core/Paper';
-import Hidden from '@material-ui/core/Hidden';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
-import Modal from '@material-ui/core/Modal';
-import { green } from '@material-ui/core/colors';
+// import { Button } from '@material-ui/core';
+// import { green } from '@material-ui/core/colors';
 
 import logo from '../logo-seven.png';
+import '../App.css';
 
-const Assistant = (props) => {
-  const { requestId, name, Product,
-  } = props;
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const Assistant = () => {
+  const classes = useStyles();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="title-waiting-view">Estás en la sala de espera para contactar a un asistente</h1>
-        <h1 className="title-waiting-view">aguarda un momento</h1>
+        <h1 className="title-waiting-view">Estás en la sala de espera para contactar a un asistente,
+          aguarda un momento.</h1>
         <br></br>
-        <button size="large" variant="contained" href="/">Ir a encuesta</button>
+        <a href="/"><button className="button-waiting-view" size="large" variant="contained">Ir a encuesta</button></a>
       </header>
     </div>
   );
--- src/pages/waiting-view.js
+++ src/pages/waiting-view.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  CssBaseline, Container,
+  CssBaseline,
 } from '@material-ui/core';
 // import { makeStyles } from '@material-ui/core/styles';
 // import RecruitingView from '../../src/recruitment-process/recruitment_view';
@@ -11,11 +11,8 @@ import Assistant from '../components/assistant';
 export default function WaitingView() {
   return (
     <>
-      <Container component="main" maxWidth="lg">
-
-        <CssBaseline />
-        <Assistant/>
-      </Container>
+      <CssBaseline />
+      <Assistant/>
     </>
   );
 }
